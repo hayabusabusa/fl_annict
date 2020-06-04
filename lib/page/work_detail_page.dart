7fe69610c69808bda45dc1f006957eb61453f228
hayabusabusa <hayabusabusaSY@gmail.com>
2020-06-04 09:52:00 +0900

    Fix work detail page

--- lib/page/work_detail_page.dart
+++ lib/page/work_detail_page.dart
@@ -24,12 +24,11 @@ class WorkDetailPage extends StatelessWidget {
         slivers: <Widget>[
           // AppBar
           SliverAppBar(
-            title: Text(work.title),
             snap: true,
             floating: true,
             pinned: true,
             stretch: true,
-            expandedHeight: 200,
+            expandedHeight: MediaQuery.of(context).size.height / 3.5, // Screen.height / 4
             flexibleSpace: FlexibleSpaceBar(
               collapseMode: CollapseMode.pin,
               background: work.image == null 
@@ -41,8 +40,52 @@ class WorkDetailPage extends StatelessWidget {
             ),
           ),
           // Body
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            // Title Section
+            sliver: SliverList(
+              delegate: SliverChildListDelegate([
+                Text(
+                  work.title,
+                  style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w500,),
+                ),
+                const SizedBox(height: 4),
+                Wrap(
+                  direction: Axis.horizontal,
+                  alignment: WrapAlignment.start,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 8,
+                  children: <Widget>[
+                    BorderedChip(text: work.media),
+                    BorderedChip(text: work.season),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      IconText(icon: Icons.info, text: work.watchersCount.toString()),
+                      const VerticalDivider(),
+                      IconText(icon: Icons.info, text: work.watchersCount.toString()),
+                    ],
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          // Divider
+          SliverToBoxAdapter(
+            child: SpacedHorizontalDivider(color: Colors.grey[200])
+          ),
+          // Episodes
           SliverFillRemaining(
-            child: Center(child: Text('Body'))
+            hasScrollBody: false,
+            child: Center(
+              child: SizedBox(width: 24, height: 24, child: CircularProgressIndicator()),
+            )
           )
         ],
       ),
