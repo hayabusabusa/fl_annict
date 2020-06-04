1822b902fbf94deed907552630fab33e37353a1a
hayabusabusa <hayabusabusaSY@gmail.com>
2020-06-05 00:31:16 +0900

    Transition with open container

--- lib/page/works_page.dart
+++ lib/page/works_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:animations/animations.dart';
 
 import 'package:fl_annict/notifier/notifier.dart';
 import 'package:fl_annict/page/page.dart';
@@ -20,14 +21,13 @@ class WorksPage extends StatelessWidget {
             itemCount: works.length,
             controller: value.scrollController,
             separatorBuilder: (_, index) => const SpacedHorizontalDivider(lSpace: 120,),  // Image 104px + Space 16px
-            itemBuilder: (_, index) => WorkListItem(
-              work: works[index], 
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => WorkDetailPage.wrapped(works[index]))
-                );
-              },
-            ),
+            itemBuilder: (_, index) => OpenContainer(
+              transitionDuration: Duration(milliseconds: 400),
+              openBuilder: (_, __) => WorkDetailPage.wrapped(works[index]),
+              closedElevation: 0,
+              closedShape: RoundedRectangleBorder(),
+              closedBuilder: (_, __) => WorkListItem(work: works[index], onTap: null,)
+            )
           ),
         )
       )
